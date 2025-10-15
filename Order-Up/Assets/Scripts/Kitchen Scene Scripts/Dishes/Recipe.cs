0e401366e659281a91e0fe20a4a9e06f9392d424
Kay <a1-6@Mac.lan>
2025-10-15 11:10:19 -0700

    resolved prefab conflicts using dev version

--- Order-Up/Assets/Scripts/Kitchen Scene Scripts/Dishes/Recipe.cs
+++ Order-Up/Assets/Scripts/Kitchen Scene Scripts/Dishes/Recipe.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
  
+[System.Serializable]
+public class RecipeIngredientRequirement
+{
+    public IngredientData ingredient;
+    public IngredientState requiredState;
+}
+
 
 // ScriptableObject to define a recipe
 [CreateAssetMenu(fileName = "New Recipe", menuName = "Scriptable Objects/Recipe")]
@@ -10,29 +17,59 @@ public class Recipe : ScriptableObject
 {
     public string dishName;
     public string ID;
-    public List<IngredientData> requiredIngredients = new List<IngredientData>();
+
+    //public List<IngredientData> requiredIngredients = new List<IngredientData>();
+    public List<RecipeIngredientRequirement> requiredIngredients;
     public GameObject dishPrefab; // The final food item to spawn
 
     public Sprite icon;
 
     // Check if the given ingredients match this recipe
-    public bool MatchesIngredients(List<Ingredient> ingredients)
+    // public bool MatchesIngredients(List<Ingredient> ingredients)
+    // {
+    //     if (ingredients.Count != requiredIngredients.Count)
+    //         return false;
+
+    //      // Extract the data from the given ingredient instances
+    //     var providedData = ingredients.Select(i => i.ingredientData).OrderBy(d => d.ingredientName).ToList();
+    //     var requiredData = requiredIngredients.OrderBy(d => d.ingredientName).ToList();
+
+
+    //     // Check if all ingredients match
+    //     for (int i = 0; i < providedData.Count; i++)
+    //     {
+    //         if (providedData[i] != requiredData[i])
+    //             return false;
+    //     }
+
+    //     return true;
+    // }
+
+    public bool MatchesIngredients(List<Ingredient> provided)
     {
-        if (ingredients.Count != requiredIngredients.Count)
+        //check the ingredient nums
+        if(provided.Count != requiredIngredients.Count)
             return false;
-
-         // Extract the data from the given ingredient instances
-        var providedData = ingredients.Select(i => i.ingredientData).OrderBy(d => d.ingredientName).ToList();
-        var requiredData = requiredIngredients.OrderBy(d => d.ingredientName).ToList();
-
-
-        // Check if all ingredients match
-        for (int i = 0; i < providedData.Count; i++)
+        
+        
+        //
+        foreach (RecipeIngredientRequirement req in requiredIngredients)
         {
-            if (providedData[i] != requiredData[i])
+            bool found = false;
+            foreach (Ingredient ing in provided)
+            {
+                Debug.Log($"Checking {ing.ingredientData.name} ({ing.currentState}) vs {req.ingredient.name} ({req.requiredState})");
+
+                if (ing.ingredientData == req.ingredient && ing.currentState == req.requiredState)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if(!found)
                 return false;
         }
-
         return true;
     }
 }
