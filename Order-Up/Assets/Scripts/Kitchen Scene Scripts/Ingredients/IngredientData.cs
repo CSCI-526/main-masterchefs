d87400d94a2cacd2ea7d610517af21d1904bcfc5
Kay <a1-6@Mac.lan>
2025-10-14 17:48:13 -0700

    add feature: setting ingredient states

--- Order-Up/Assets/Scripts/Kitchen Scene Scripts/Ingredients/IngredientData.cs
+++ Order-Up/Assets/Scripts/Kitchen Scene Scripts/Ingredients/IngredientData.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 
+
+public enum IngredientState {Raw, Cooked, Overcooked}
+
 [CreateAssetMenu(fileName = "IngredientData", menuName = "Scriptable Objects/Ingredient")]
+
 public class IngredientData : ScriptableObject
 {
     public string ingredientName;
     public Sprite icon;
     public IngredientData cookedResult; // What this ingredient becomes when cooked
+    public IngredientData overcookedResult;//What this becomes when overcooked
+
 }
