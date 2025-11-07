9a6e6dfa8e893ebf81aa82fb32c6ffdc8096b8a1
WYD <wuyadong356784@gmail.com>
2025-11-06 18:43:54 -0800

    new branch update

--- Order-Up/Assets/Scripts/Managers/Submit.cs
+++ Order-Up/Assets/Scripts/Managers/Submit.cs
@@ -4,6 +4,7 @@ public class SubmitButton : MonoBehaviour
 {
     public RatingSystem ratingSystem;
 
+
     // Connect this to your button's OnClick event in the Inspector
     public void OnSubmitClicked()
     {
